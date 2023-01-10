5fd0ad5858e85f5f1e84022303a48c0ae4ab9fd4
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 13:31:32 -0300

    Refactor App.js
    So the user can change between Timer and Settings page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Settings } from './components/Settings/Settings';
 import Timer from './components/Timer/Timer';
+import {useState} from 'react';
 
 function App() {
+  const [showSettings, setShowSettings] = useState(true);
   return (
     <main>
-      <Settings/>
-      <Timer/>
+      {showSettings? <Settings/> : <Timer/> }
     </main>
   );
 }
