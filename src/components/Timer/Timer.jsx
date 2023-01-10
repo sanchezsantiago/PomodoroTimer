6f908e7f6962500a2b4bf85875e3cfbd2ecbea22
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 13:12:38 -0300

    Add SettingsButton Component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -4,6 +4,7 @@ import 'react-circular-progressbar/dist/styles.css';
 import React from 'react'
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
 
 const red = '#f54e4e';
 const green = '#4aec8c';
@@ -21,6 +22,9 @@ const Timer = () => {
         <div style={{marginTop:'20px'}}>
             <PlayButton/>
             <PauseButton/>
+        </div>
+        <div style={{marginTop:'20px'}}>
+            <SettingsButton/>
         </div>
     </div>
   )
