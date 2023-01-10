bb21af0d8286c77c8c343f1c2791c2c805f90689
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 13:35:33 -0300

    Import react-slider in Settings.jsx

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import ReactSlider from 'react-slider'
+
 
 export const Settings = () => {
   return (
     <div style={{textAling: 'left'}}>
         <label>Work minutes: </label>
+        <ReactSlider
+            className={'slider'}
+            trackClassName={"track"}
+            value={45}
+            min={1}
+            max={120}
+        />
         <label>Break minutes: </label>
     </div>
   )
