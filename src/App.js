f223ba3afbe9f84eecaf3d4617ef5ebb5d5a1ee8
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 13:18:48 -0300

    Create Settings Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Settings } from './components/Settings/Settings';
 import Timer from './components/Timer/Timer';
 
 function App() {
   return (
     <main>
+      <Settings/>
       <Timer/>
     </main>
   );
