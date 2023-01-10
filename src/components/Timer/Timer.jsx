3893898ff9db3e1b6e169b2335d5af7414b68415
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 12:50:58 -0300

    Test circular progressbar in Timer Component

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,8 +1,22 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 import React from 'react'
+
+const red = '#f54e4e';
+const green = '#4aec8c';
 
 const Timer = () => {
   return (
-    <div>Timer</div>
+    <div>
+        <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+            rotation:'0',
+            strokeLinecap:'round',
+            textColor: '#fff',
+            pathColor: red,
+            trailColor: 'rgba(255,255,255,.2)',
+        })} />
+    </div>
   )
 }
 
