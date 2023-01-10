6903fe1d79e3a6017497273b68a145ac3d5c1ae5
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-10 13:29:06 -0300

    Refactor Settings Component

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Settings = () => {
   return (
-    <div>Settings</div>
+    <div style={{textAling: 'left'}}>
+        <label>Work minutes: </label>
+        <label>Break minutes: </label>
+    </div>
   )
 }
