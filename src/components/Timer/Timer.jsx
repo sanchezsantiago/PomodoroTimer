b732eaef6b0d25d14039933ac4f6e6b6e7b86964
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-11 19:36:03 -0300

    Build Timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -5,17 +5,63 @@ import React from 'react'
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import SettingsContext from '../Settings/SettingsContext';
 const red = '#f54e4e';
 //const green = '#4aec8c';
 
 const Timer = () => {
   const settingsInfo = useContext(SettingsContext);
-  const [isPaused, setIsPaused] = useState(true)
+  const [isPaused, setIsPaused] = useState(true);
+  const [secondsLeft, setSecondsLeft] = useState(0)
+  const [mode, setMode] = useState('Work');
+  
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+
+  function initTimer() {
+    setSecondsLeft(settingsInfo.workMinutes * 60);
+  }
+  function switchMode() {
+    const nextMode = modeRef.current === 'work' ? 'break' : 'work';
+    const nextSeconds =  (nextMode === 'work'? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+    setMode(nextMode);
+    setSecondsLeft(nextSeconds);
+    secondsLeftRef.current = nextSeconds;
+  }
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  useEffect(()=>{
+    initTimer();
+
+    const interval = setInterval(()=>{
+      if(isPausedRef.current){
+        return;
+      }
+      if(secondsLeftRef.current === 0){
+        return switchMode();
+      }
+      tick();
+    }, 1000)
+    return clearInterval(interval);
+  },[settingsInfo])
+
+  const totalSeconds = mode === 'work' ? 
+    settingsInfo.workMinutes * 60 
+    : settingsInfo.breakMinutes * 60;
+    
+  const porcentage = Math.round(secondsLeft / totalSeconds) * 100;
+  const minutes = Math.floor(secondsLeft / 60);
+  const seconds = secondsLeft % 60;
+
   return (
     <div>
-        <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
+        <CircularProgressbar value={porcentage} text={minutes + ':' + seconds} styles={buildStyles({
             rotation:'0',
             strokeLinecap:'round',
             textColor: '#fff',
