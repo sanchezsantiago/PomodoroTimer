04e4987623adff67d4158a63802f709a146b9f9a
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-11 18:51:39 -0300

    Add isPaused Hook

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -5,13 +5,14 @@ import React from 'react'
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import SettingsContext from '../Settings/SettingsContext';
 const red = '#f54e4e';
 //const green = '#4aec8c';
 
 const Timer = () => {
   const settingsInfo = useContext(SettingsContext);
+  const [isPaused, setIsPaused] = useState(true)
   return (
     <div>
         <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
@@ -22,8 +23,7 @@ const Timer = () => {
             trailColor: 'rgba(255,255,255,.2)',
         })} />
         <div style={{marginTop:'20px'}}>
-            <PlayButton/>
-            <PauseButton/>
+          {isPaused? <PlayButton/> : <PlayButton/>}
         </div>
         <div style={{marginTop:'20px'}}>
             <SettingsButton onClick={()=> settingsInfo.setShowSettings(true)}/>
