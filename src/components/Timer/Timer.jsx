a968c35f91fa28cfcace510f49acc54f6efd7c20
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-11 17:27:47 -0300

    Make SettingsButton works
    Pass context to Timer so the component can change the state of the Hook

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -5,11 +5,13 @@ import React from 'react'
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 import SettingsButton from './SettingsButton';
-
+import { useContext } from 'react';
+import SettingsContext from '../Settings/SettingsContext';
 const red = '#f54e4e';
-const green = '#4aec8c';
+//const green = '#4aec8c';
 
 const Timer = () => {
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div>
         <CircularProgressbar value={60} text={`60%`} styles={buildStyles({
@@ -24,7 +26,7 @@ const Timer = () => {
             <PauseButton/>
         </div>
         <div style={{marginTop:'20px'}}>
-            <SettingsButton/>
+            <SettingsButton onClick={()=> settingsInfo.setShowSettings(true)}/>
         </div>
     </div>
   )
