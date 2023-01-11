45103eb22e4f641b61cae9023a078afef87ec822
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-11 17:03:15 -0300

    Test SettingsContext in Settings Components

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import ReactSlider from 'react-slider'
+import SettingsContext from './SettingsContext'
+import { useContext } from 'react'
 import './slider.css'
 
 export const Settings = () => {
+  const settingsInfo = useContext(SettingsContext);
   return (
     <div style={{textAling: 'left'}}>
-        <label>Work minutes: </label>
+        <label>Work: {settingsInfo.workMinutes}:00 </label>
         <ReactSlider
             className={'slider'}
             thumbClassName = {'thumb'}
             trackClassName={"track"}
-            value={45}
+            value={settingsInfo.workMinutes}
             min={1}
             max={120}
         />
-        <label>Break minutes: </label>
+        <label>Break: {settingsInfo.breakMinutes}:00 </label>
         <ReactSlider
             className={'slider'}
             thumbClassName = {'thumb'}
             trackClassName={"track"}
-            value={45}
+            value={settingsInfo.breakMinutes}
             min={1}
             max={120}
         />
