b77d8ed71758b1c5c748a7fb52160637d8c767e7
Santiago Sanchez <santiagosancheznieva@gmail.com>
2023-01-11 16:59:34 -0300

    Add SettingsContext in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import { Settings } from './components/Settings/Settings';
 import Timer from './components/Timer/Timer';
 import {useState} from 'react';
+import SettingsContext from './components/Settings/SettingsContext';
 
 function App() {
   const [showSettings, setShowSettings] = useState(true);
   return (
     <main>
-      {showSettings? <Settings/> : <Timer/> }
+      <SettingsContext.Provider value={{
+        workMinutes: 45,
+        breakMinutes: 15,
+      }}>
+
+        {showSettings? <Settings/> : <Timer/> }
+
+      </SettingsContext.Provider>
     </main>
   );
 }
